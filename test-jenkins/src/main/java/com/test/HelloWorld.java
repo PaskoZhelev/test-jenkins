b88e0d7ff6ff630d46ed3fc05ffd99c250abd829
PaskoZhelev <pasko92@abv.bg>
2017-12-01 13:43:13 +0100

    test change

--- test-jenkins/src/main/java/com/test/HelloWorld.java
+++ test-jenkins/src/main/java/com/test/HelloWorld.java
@@ -1,6 +1,13 @@
 package com.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HelloWorld {
+	
+	@GetMapping("/")
+	public String getIndex() {
+		return "home";
+	}
 	
 	public String getHelloWorld() {
 		return "Hello World!";
