f18854ea2e686ee0801ad5128103fd4f129898c1
PaskoZhelev <pasko92@abv.bg>
2017-12-01 10:52:45 +0100

    Hello World test

---
+++ test-jenkins/src/main/java/com/test/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class HelloWorld {
+	
+	public String getHelloWorld() {
+		return "Hello World!";
+	}
+}
---
+++ test-jenkins/src/test/java/com/test/HelloWorldTest.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HelloWorldTest {
+
+	@InjectMocks
+	private HelloWorld cut;
+	
+	@Before
+	public void setUp() throws Exception {
+		cut = new HelloWorld();
+	}
+
+	@Test
+	public void testGetHelloWorld() {
+		assertEquals("Hello World!", cut.getHelloWorld());
+	}
+
+}
